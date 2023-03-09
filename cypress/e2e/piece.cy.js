b4387ed4ed4a73fe193b2d39b1fbfebc3d9c39f8
TheVirtuoid <thevirtuoid@gmail.com>
2023-03-09 13:47:00 -0500

    Completed piece unit test

--- cypress/e2e/piece.cy.js
+++ cypress/e2e/piece.cy.js
@@ -1,5 +1,6 @@
-import Piece from "../../src/js/classes/Piece.js";
 import Vertex from "../../src/js/classes/Vertex.js";
+import Piece from "../../src/js/classes/Piece.js";
+
 
 describe('Piece: ', () => {
 
@@ -7,69 +8,268 @@ describe('Piece: ', () => {
 			const piece = new Piece({ id: <someId> });
 	 */
 	describe('creating default: ', () => {
-		it('should throw error if no id is specified', () => {});
-		it('should create if id only specified', () => {});
-		it('should default to no vertices', () => {});
-		it('should throw error as id cannot be changed', () => {});
-		it('should throw error as no property can be added', () => {});
+		it('should throw error if no id is specified', () => {
+			try {
+				const vertex = new Piece();
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should create if id only specified', () => {
+			const piece = new Piece({ id: 'a' });
+			expect(piece instanceof Piece).to.be.true;
+		});
+		it('should default to no vertices', () => {
+			const piece = new Piece({ id: 'a' });
+			expect(vertexCount).to.equal(0);
+		});
+		it('should throw error as id cannot be changed', () => {
+			const piece = new Piece({ id: 'a' });
+			try {
+				piece.id = 'b';
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should throw error as no property can be added', () => {
+			const piece = new Piece({ id: 'a' });
+			try {
+				piece.newProperty = true;
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
 	});
 
 	/*
 		const piece = new Piece({ id: <someId>, vertices: [vtx1, vtx2, ... vtxZ] });
 	 */
 	describe('creating with vertices property: ', () => {
-		it('should throw error if vertices argument is not array or vertex object', () => {});
-		it('should return the correct number of vertices', () => {});
-		it('should throw error as vertices property cannot be changed', () => {});
+		it('should throw error if vertices argument is not array or vertex object', () => {
+			try {
+				const piece = new Piece({ id: 'a', vertices: 'badone' });
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should return the correct number of vertices', () => {
+			const vertex = new Vertex({ id: 'v1' });
+			const piece1 = new Piece({ id: 'a', vertices: [vertex]});
+			expect(piece1.vertexCount).to.equal(1);
+		});
+		it('should throw error as vertices property cannot be changed', () => {
+			const piece = new Piece({ id: 'a', vertices: [] });
+			try {
+				piece.vertices = 'badone';
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
 	});
 
 	/*
 			piece.addVertex(vtx);
 	 */
 	describe('addVertex: ', () => {
-		it('should throw error if argument is not a Vertex', () => {});
-		it('should throw error if vertex is already in the collection', () => {});
-		it('should add the vertex to the collection', () => {});
-		it('should return the piece', () => {});
+		it('should throw error if argument is not a Vertex', () => {
+			const piece = new Piece({ id: 'a', vertices: [] });
+			try {
+				piece.addVertex('badone');
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should throw error if vertex is already in the collection', () => {
+			const vertex = new Vertex({ id: 'v' });
+			const piece = new Piece({ id: 'a', vertices: [vertex] });
+			try {
+				piece.addVertex(vertet);
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should add the vertex to the collection', () => {
+			const vertex = new Vertex({ id: 'v' });
+			const piece = new Piece({ id: 'a', vertices: [] });
+			piece.addVertex(vertex);
+			expect(piece.vertexCount).to.equal(1);
+		});
+		it('should return the piece', () => {
+			const vertex = new Vertex({ id: 'v' });
+			const piece = new Piece({ id: 'a', vertices: [] });
+			const newPiece = piece.addVertex(vertex);
+			expect(newPiece === piece).to.be.true;
+		});
 	});
 
 	/*
 			piece.addVertices([vtx1, vtx2, ..., vtxZ]);
 	 */
 	describe('addVertices: ', () => {
-		it('should throw error if argument is not an array of Vertices', () => {});
-		it('should throw error if at least one of the vertices is in the collection', () => {});
-		it('given at least one vertices is in the collection, should not add ANY vertices', () => {});
-		it('should add the vertices to the collection', () => {});
-		it('should return the piece', () => {});
+		it('should throw error if argument is not an array of Vertices', () => {
+			const piece = new Piece({ id: 'a', vertices: [] });
+			try {
+				piece.addVertices('badone');
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should throw error if at least one of the vertices is in the collection', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2 '});
+			const piece = new Piece({ id: 'a', vertices: [vertex2] });
+			try {
+				piece.addVertices([vertex1, vertex2]);
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('given at least one vertices is in the collection, should not add ANY vertices', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2 '});
+			const piece = new Piece({ id: 'a', vertices: [vertex2] });
+			try {
+				piece.addVertices([vertex1, vertex2]);
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(piece.vertexCount).to.equal(1);
+			}
+		});
+		it('should add the vertices to the collection', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2 '});
+			const piece = new Piece({ id: 'a', vertices: [] });
+			piece.addVertices([vertex1, vertex2]);
+			expect(piece.vertexCount).to.equal(2);
+		});
+		it('should return the piece', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2 '});
+			const piece = new Piece({ id: 'a', vertices: [] });
+			const newPiece = piece.addVertices([vertex1, vertex2]);
+			expect(newPiece === piece).to.be.true;
+		});
 	});
 
 	/*
 			piece.removeVertex(vtx);
 	 */
 	describe('removing a vertex: ', () => {
-		it('should throw error if argument is not a vertex', () => {});
-		it('should remove the vertex', () => {});
-		it('should return the piece', () => {});
-		it('should throw error if vertex cannot be found in the collection', () => {});
+		it('should throw error if argument is not a vertex', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1] });
+			try {
+				piece.removeVertex('badone');
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should remove the vertex', () => {
+			const vertex = new Vertex({ id: 'v1' });
+			const piece = new Piece({ id: 'a', vertices: [vertex] });
+			piece.removeVertex(vertex);
+			expect(piece.vertexCount).to.equal(0);
+		});
+		it('should return the piece', () => {
+			const vertex = new Vertex({ id: 'v1' });
+			const piece = new Piece({ id: 'a', vertices: [vertex] });
+			const newPiece = piece.removeVertex(vertex);
+			expect(newPiece === piece).to.be.true;
+		});
+		it('should throw error if vertex cannot be found in the collection', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1] });
+			try {
+				piece.removeVertex(vertex2);
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
 	});
 
 	/*
 			piece.removeVertices([vtx1, vtx2, ..., vtxZ]);
 	 */
 	describe('removeVertices: ', () => {
-		it('should throw error if argument is not an array of vertices', () => {});
-		it('should remove all the vertices', () => {});
-		it('should return the piece', () => {});
-		it('should throw error if at least one of the vertices cannot be found in the collection', () => {});
-		it('if at least one vertex cannot be found, should NOT remove any vertices', () => {});
+		it('should throw error if argument is not an array of vertices', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1] });
+			try {
+				piece.removeVertices('badone');
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should remove all the vertices', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1, vertex2] });
+			piece.removeVertices([vertex1, vertex2]);
+			expect(piece.vertexCount).to.equal(0);
+		});
+		it('should return the piece', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1, vertex2] });
+			const newPiece = piece.removeVertices([vertex1, vertex2]);
+			expect(newPiece === piece).to.be.true;
+		});
+		it('should throw error if at least one of the vertices cannot be found in the collection', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1] });
+			try {
+				piece.removeVertices([vertex1, vertex2]);
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('if at least one vertex cannot be found, should NOT remove any vertices', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1] });
+			try {
+				piece.removeVertices([vertex1, vertex2]);
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(piece.vertexCount).to.equal(1);
+			}
+		});
 	});
 
 	/*
 			piece.clearVertices();
 	 */
 	describe('clearVertices: ', () => {
-		it('should clear all vertices', () => {});
+		it('should clear all vertices', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1, vertex2] });
+			piece.clearVertices();
+			expect(piece.vertexCount).to.equal(0);
+		});
+		it('should return the piece', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece = new Piece({ id: 'a', vertices: [vertex1, vertex2] });
+			const newPiece = piece.clearVertices();
+			expect(newPiece === piece).to.be.true;
+		});
 	});
 
 	/*
@@ -79,19 +279,70 @@ describe('Piece: ', () => {
 			We return either an array of vertices if matched, or Boolean false if not matched.
 	 */
 	describe('isConnectedTo: ', () => {
-		it('should throw error if secondPiece argument is not a Piece', () => {});
-		it('should return false if the two pieces are not connected', () => {});
-		it('should return array of vertices if pieces are connected', () => {});
+		it('should throw error if secondPiece argument is not a Piece', () => {
+			const piece = new Piece({ id: 'a', vertices: [] });
+			try {
+				piece.isConnectedTo('badone');
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should return false if the two pieces are not connected', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece1 = new Piece({ id: 'a', vertices: [vertex1] });
+			const piece2 = new Piece({ id: 'b', vertices: [vertex2] });
+			expect(piece1.isConnectedTo(piece2)).to.be.false;
+		});
+		it('should return array of vertices if pieces are connected', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const piece1 = new Piece({ id: 'a', vertices: [vertex1] });
+			const piece2 = new Piece({ id: 'b', vertices: [vertex1, vertex2] });
+			expect(piece1.isConnectedTo(piece2).length).to.equal(1);
+		});
 	});
 
 	/*
 			const newPiece = piece.merge(secondPiece)
 	 */
 	describe('merge: ', () => {
-		it('should throw error if secondPiece argument is not a Piece', () => {});
-		it('should return a new piece with all the vertices combined', () => {});
-		it('should clear all vertices from piece', () => {});
-		it('should clear all vertices from secondPiece', () => {});
+		it('should throw error if secondPiece argument is not a Piece', () => {
+			const piece = new Piece({ id: 'a', vertices: [] });
+			try {
+				piece.merge('badone');
+				expect(true).to.be.false;
+			} catch (err) {
+				expect(err.name).to.equal('TypeError');
+			}
+		});
+		it('should return a new piece with all the vertices combined', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const vertex3 = new Vertex({ id: 'v3' });
+			const piece1 = new Piece({ id: 'p1', vertices: [vertex1, vertex2]});
+			const piece2 = new Piece({ id: 'p2', vertices: [vertex2, vertex3]});
+			const newPiece = piece2.merge(piece2);
+			expect(newPiece.vertexCount).to.equal(3);
+		});
+		it('should clear all vertices from piece', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const vertex3 = new Vertex({ id: 'v3' });
+			const piece1 = new Piece({ id: 'p1', vertices: [vertex1, vertex2]});
+			const piece2 = new Piece({ id: 'p2', vertices: [vertex2, vertex3]});
+			const newPiece = piece2.merge(piece2);
+			expect(piece1.vertexCount).to.equal(0);
+		});
+		it('should clear all vertices from secondPiece', () => {
+			const vertex1 = new Vertex({ id: 'v1' });
+			const vertex2 = new Vertex({ id: 'v2' });
+			const vertex3 = new Vertex({ id: 'v3' });
+			const piece1 = new Piece({ id: 'p1', vertices: [vertex1, vertex2]});
+			const piece2 = new Piece({ id: 'p2', vertices: [vertex2, vertex3]});
+			const newPiece = piece2.merge(piece2);
+			expect(piece2.vertexCount).to.equal(0);
+		});
 	});
-
 });
